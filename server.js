56ec0e4ce53aa9ae3f64c39496694580905a9d98
BroCodes20 <bhp9697@gmail.com>
2025-06-20 12:58:25 +0530

    Add files via upload
    Setup weather backend

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const axios = require("axios");
+require("dotenv").config;
+const cors = require("cors");
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(cors());
+
+app.get("/weather", async (req, res) => {
+  const { lat, lon } = req.query;
+
+  if (!lat || !lon) {
+    return res.status(400).json({
+      error: "Latitude and Longitude are required",
+    });
+  }
+
+  try {
+    const response = await axios.get(
+      "https://api.openweathermap.org/data/2.5/weather",
+      {
+        params: {
+          lat,
+          lon,
+          units: "metric",
+          appid: process.env.WEATHER_API_KEY,
+        },
+      }
+    );
+
+    const data = response.data;
+    res.json({
+      temp: data.main.temp,
+      description: data.weather[0].description,
+      city: data.name,
+    });
+  } catch (err) {
+    res.status(500).json({ error: "Unable to fetch weather data" });
+  }
+});
